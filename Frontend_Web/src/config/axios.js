d6fe9b37f981a5b2714198ace2de53de32a9aaca
Zgustavoz <gustavoibarra323@gmail.com>
2026-02-16 14:15:05 -0400

    cambios para subir a produccion el frontend

--- Frontend_Web/src/config/axios.js
+++ Frontend_Web/src/config/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instancia = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // Sin /api al final
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
