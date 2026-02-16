4e3484bb5c8e13d131333e7052d79f0c7c2f9172
Zgustavoz <gustavoibarra323@gmail.com>
2026-02-16 12:50:13 -0400

    arreglando errores

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-# import os
+import os
 import dj_database_url
 from pathlib import Path
 from decouple import config
@@ -21,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+
+PORT = os.environ.get('PORT', 10000)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
