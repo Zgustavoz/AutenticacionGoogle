fcfa8b0f1b14c08240861d4f9de26168190125ec
Zgustavoz <gustavoibarra323@gmail.com>
2026-02-17 10:06:14 -0400

    z11

--- Frontend_Web/src/Auth/components/LoginGoogle.jsx
+++ Frontend_Web/src/Auth/components/LoginGoogle.jsx
@@ -2,21 +2,26 @@
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import toast from 'react-hot-toast';
 
-// const GOOGLE_CLIENT_ID = import.meta.env.GOOGLE_CLIENT_ID
-const GOOGLE_CLIENT_ID = '395832852196-dmo7tvcbi8acsutcf7s7a2cpcqv21298.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+// const GOOGLE_CLIENT_ID = '395832852196-dmo7tvcbi8acsutcf7s7a2cpcqv21298.apps.googleusercontent.com';
 
 export const LoginGoogle = ({ onSuccess }) => {
   return (
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <GoogleLogin
-        onSuccess={onSuccess}
-        onError={() => toast.error('Error con Google Login')}
-        theme="outline"
-        size="large"
-        text="continue_with"
-        shape="rectangular"
-        width="384"
-      />
+      <div className="w-full flex justify-center">
+        <GoogleLogin
+          onSuccess={onSuccess}
+          onError={() => toast.error('Error con Google Login')}
+          theme="outline"
+          size="large"
+          text="continue_with"
+          shape="rectangular"
+          width="100%" // Cambiamos a 100% para que sea responsive
+          containerProps={{
+            className: "w-full max-w-[384px] md:max-w-[400px] lg:max-w-[450px]"
+          }}
+        />
+      </div>
     </GoogleOAuthProvider>
   );
 };
