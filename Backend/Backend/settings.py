948df9818bff56eab2445e991fdda0f54bb895bf
Zgustavoz <gustavoibarra323@gmail.com>
2026-02-16 22:40:15 -0400

    z

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -172,7 +172,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    config('FRONTEND_URL'),  # <-- DESCOMENTAR EN PRODUCCIÓN
+    "https://autenticacion-google-eight.vercel.app",  # <-- DESCOMENTAR EN PRODUCCIÓN
     # "https://www.autenticacion-google-eight.vercel.app",
 ]
 
